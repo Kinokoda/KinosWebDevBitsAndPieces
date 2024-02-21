29e6801204649a108ff051f1115f5cd1abbcabeb
Choco <pergantis.stratos05@gmail.com>
2024-02-22 00:02:50 +0200

    Fixed showing hidden files

--- phpFileIndex/index.php
+++ phpFileIndex/index.php
@@ -1,13 +1,13 @@
 <!-- PHP Based file index creator. Made by Kinokoda. -->
 
 <?php
-
 $fileList = scandir('./'); #Scan the current directory for files and folders
 $tempArr = explode('/', getcwd());
 echo "<h1 style = 'text-align: center;'>Index of ".$tempArr[count($tempArr)-1]."</h1>"; #Title of the page
 for ($i = 0; $i < count($fileList); $i++)  #Loop through the array of files and folders
 {
-    if(is_dir($fileList[$i]) && $fileList[$i] != '.')  #If the current item is a folder...
+    $hideCheck = $fileList[$i];
+    if(is_dir($fileList[$i]) && $fileList[$i] != '.' && $hideCheck[0] != '.')  #If the current item is a folder...
     {
         if ($fileList[$i] == '..')  #and if it is the parent
         {
