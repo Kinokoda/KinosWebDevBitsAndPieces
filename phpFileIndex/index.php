ccb8a656a574bc25ff155e46c6edaf84dcf53df1
Choco <pergantis.stratos05@gmail.com>
2024-02-21 22:14:53 +0200

    version 1.0 of fileIndexer

--- phpFileIndex/index.php
+++ phpFileIndex/index.php
@@ -1,19 +1,55 @@
 <?php
 
 $fileList = scandir('./'); #Scan the current directory for files and folders
+$tempArr = explode('/', getcwd());
+echo "<h1 style = 'text-align: center;'>Index of ".$tempArr[count($tempArr)-1]."</h1>"; #Title of the page
 for ($i = 0; $i < count($fileList); $i++)  #Loop through the array of files and folders
 {
     if(is_dir($fileList[$i]) && $fileList[$i] != '.')  #If the current item is a folder...
     {
         if ($fileList[$i] == '..')  #and if it is the parent
         {
-            echo "<a href='../'>Go Back</a><br>"; 
+            echo "<a href='../' >Go Back</a><br>"; 
         }
-
-    }elseif(is_file($fileList[$i]))
+        
+        else
+        {
+        echo "<p><a href='./$fileList[$i]'><b><span>[DIR]</span></b> $fileList[$i]</a></p>";
+        }
+    }
+    
+    elseif(is_file($fileList[$i]))
     {
-        echo "$fileList[$i] is a file <br>";
+        if ($fileList[$i] != "index.php")
+        {
+            echo "<a href='./$fileList[$i]'><span>[FILE]</span> $fileList[$i]</a><br>";
+        }
+        
     }
 }
 
 ?>
+
+<style> /*Styling the page*/
+    span
+    {
+        color: green;
+    }
+    a:link
+    {
+        color: ;
+    }
+    a:visited
+    {
+        color: purple;
+        
+    }
+    a:active
+    {
+        color: red;
+    }
+    a:hover
+    {
+        color: orange;
+    }
+</style>
